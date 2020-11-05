63933687ebf76634ba9c86eca7efbdfe0b94af77
Sylva <sylvaekpendu@gmail.com>
2020-11-04 20:40:01 -1200

    my first deployment

---
+++ plants/index.php
@@ -0,0 +1,153 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="css/boostrap.min.css"> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+        crossorigin="anonymous"></script>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+
+
+</head>
+
+<body>
+<div class="logo">
+  <h1 class="logo-2">HOUSE <span>of </span>PLANTS</h1>
+</div>
+
+<div class="container-fluid">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#" type="search" placeholder="Search" aria-label="Search"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon fa fa-search icon"></span>
+  </button>
+
+
+  <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" width="100px"type="submit">Search</button>
+    </form>
+
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
+</div>
+
+<div class="container">
+
+<div class="row">
+<div class="col-md-6">
+<div class="monst" mt-8>
+<img src="img/pl.png" width="450px"alt="">
+</div>
+</div>
+
+<div class="col-md-6">
+<h6 class="pedia">-- PLANTOPEDIA</h56>
+
+<div class="text">
+<h1 class="text">Monstera <br> Deliciosa</h1>
+<h5 class="aka">AKA SWISS CHEESE PLANT</h5>
+
+<h5 class="lorem">Best known as an ornamental, R. regia is also used as a source of thatch, construction timber, and in some forms of so-called traditional medicine,
+although there is currently no valid scientific evidence to support the efficacy or use of any palm species for medicinal purposes. 
+    Its flowers are visited by birds and bats, and it serves as a roosting site and food source for a variety of animals. 
+ Roystonea regia is the national tree of Cuba, and has a religious role both in Santería and Christianity, 
+where it is used in Palm Sunday observances</h5>
+ <button class="btn btn btn-warning">MORE INFO</button>
+<button class="btn-1 btn btn-dark" >A-Z INDEX</button>
+</div>
+</div>
+
+
+<div onclick=before(); class="arrow">
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-bar-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5zM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5z"/>
+                            </svg>
+                        </div>
+                        <div onclick=afterr(); class="arrow-2">
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-bar-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8zm-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5z"/>
+                            </svg>
+                        </div>
+                </div>
+            </div>
+    </section>
+    <script> 
+            var text = ` Best known as an ornamental, R. regia is also used as a source of thatch, construction timber, and in some forms of so-called traditional medicine,
+                         although there is currently no valid scientific evidence to support the efficacy or use of any palm species for medicinal purposes. 
+                         Its flowers are visited by birds and bats, and it serves as a roosting site and food source for a variety of animals. 
+                        Roystonea regia is the national tree of Cuba. `;
+
+            var type = 'The plant may be confused with Philodendron bipinnatifidum as they have similar leaves and growing habits. However, the ingestion of Philodendron bipinnatifidum may cause irritation to the digestive tract and will induce internal swelling. The sap is also known to irritate the skin.'
+
+         function before(){ 
+             document.getElementById('myImage') 
+             .src="img/royal.png"; 
+            //  document.getElementById('message') 
+            //  .innerHTML="message"; 
+             document.getElementById('newText')
+             .innerHTML=text;
+         } 
+           
+         function afterr(){ 
+             document.getElementById('myImage') 
+             .src="img/royal-p.png"; 
+             document.getElementById('newText') 
+             .innerHTML=type; 
+         } 
+        </script> 
+  </div>
+
+<section>
+
+  <footer>
+<div class="footer-section">
+<button class="foot">BRIGHT INDIRECT LIGHT</button>
+<button class="foot"> AVERAGE WARMTH</button>
+<button class="foot">WATER WELL </button>
+<button class="foot btn btn-success">BUYING OPTION </button>
+</div>
+</div>
+<center>
+<p class="sylv">Created by Sylva Ekpendu  All rights reserved &copy;2020</p>
+
+</center>
+</footer>
+</section>
